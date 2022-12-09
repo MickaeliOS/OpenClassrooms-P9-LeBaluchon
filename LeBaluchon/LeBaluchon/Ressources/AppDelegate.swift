768c0fe2dc5022c9c5b26eb7700dbf2a5cfb6bfa
Horn Mickaël <mickael.horn.pro@gmail.com>
2022-12-09 10:21:57 +0100

    Première version fonctionnelle.

--- LeBaluchon/LeBaluchon/Ressources/AppDelegate.swift
+++ LeBaluchon/LeBaluchon/Ressources/AppDelegate.swift
@@ -5,15 +5,14 @@
 //  Created by Mickaël Horn on 19/10/2022.
 //
 
-// Weather : <a href="https://www.flaticon.com/free-icons/weather" title="weather icons">Weather icons created by kosonicon - Flaticon</a>
-// Translate : <a href="https://www.flaticon.com/fr/icones-gratuites/traduction" title="traduction icônes">Traduction icônes créées par Freepik - Flaticon</a>
 // Exchange : <a href="https://www.flaticon.com/free-icons/convert" title="convert icons">Convert icons created by NewsIcons - Flaticon</a>
+// Exchange arrows : https://www.veryicon.com/icons/miscellaneous/smarteditor/exchange-13.html
+// Translate : <a href="https://www.flaticon.com/fr/icones-gratuites/traduction" title="traduction icônes">Traduction icônes créées par Freepik - Flaticon</a>
 // US Flag : https://www.freeflagicons.com/country/united_states_of_america/round_icon/
 // EU Flag : https://www.freeflagicons.com/country/european_union/round_icon/
 // FR Flag : https://www.freeflagicons.com/country/france/round_icon/
-// Exchange arrows : https://www.veryicon.com/icons/miscellaneous/smarteditor/exchange-13.html
-
-// w3KstM52K0o1fGMtkNfRJn1cmB0Mbt5p
+// Weather : <a href="https://www.flaticon.com/free-icons/weather" title="weather icons">Weather icons created by kosonicon - Flaticon</a>
+// Weather icons : https://openweathermap.org/weather-conditions
 
 import UIKit
 
